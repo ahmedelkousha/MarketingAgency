f5c95893d0673d498f698af5c80bd2dd70343501
ahmedelkousha <elkoushaservice@gmail.com>
2024-12-05 20:50:57 +0200

    new commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
-
+import HeroSection from "./components/heroSection";
 export default function Home() {
   return (
-    <div className="bg-black text-white text-center text-4xl p-6">
-      <h2>Marketing Agency</h2>
+    <div className=" text-white text-center text-4xl p-6">
+      <h2 className="bg-black">Marketing Agency</h2>
+      <HeroSection />
     </div>
   );
 }
