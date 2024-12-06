081765460a8764c8c1a3ccc4db1ea790f2abd646
Ahmed Elkousha <elkoushaservice@gmail.com>
2024-12-06 09:50:07 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Services from "./components/services"
 export default function Home() {
   return (
     <div >
-      <h2 className="bg-black">Marketing Agency</h2>
       <HeroSection />
       <Services />
     </div>
