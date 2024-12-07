c3c95c8c26681560cd2f7a853a8bfeac985f8d86
ahmedelkousha <elkoushaservice@gmail.com>
2024-12-07 09:15:16 +0200

    from 100vw to 100%

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,6 +1,31 @@
 
 export default function Header() {
   return (
-    <div>header</div>
+    <header className="bg-white flex flex-row justify-center items-center">
+      <h1 className="text-[26px] font-[700]">
+        DWS
+      </h1>
+      <nav className="flex flex-row justify-center items-center text-[14px]">
+
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Pricing</a>
+
+        <div>
+          <a href="#">Megamenu</a>
+          <div className="hidden">
+            <a href="#">Content Writing</a>
+            <a href="#">Web Development</a>
+            <a href="#">Marketing</a>
+            <a href="#">UI UX</a>
+          </div>
+        </div>
+
+        <a href="#">Blog</a>
+        <a href="#">Contact</a>
+        <button>Try free</button>
+      </nav>
+    </header>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import HeroSection from "./components/heroSection";
+import Header from "./components/header";
 import Services from "./components/services";
 export default function Home() {
   return (
     <div>
+      <Header/>
       <HeroSection />
       <Services/>
     </div>
