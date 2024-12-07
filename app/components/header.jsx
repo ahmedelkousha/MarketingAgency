c5691fef34dd2121e5104ffc877df454b260e8f0
ahmedelkousha <elkoushaservice@gmail.com>
2024-12-07 10:27:25 +0200

    header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,20 +1,20 @@
 
 export default function Header() {
   return (
-    <header className="bg-white flex flex-row justify-center items-center">
+    <header className="bg-white flex flex-row justify-evenly items-center h-20">
       <h1 className="text-[26px] font-[700]">
         DWS
       </h1>
-      <nav className="flex flex-row justify-center items-center text-[14px]">
+      <nav className="flex flex-row justify-center items-center text-[14px] gap-6 tracking-widest">
 
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Pricing</a>
+        <a className="hover:text-blue-600" href="#">Home</a>
+        <a className="hover:text-blue-600" href="#">About</a>
+        <a className="hover:text-blue-600" href="#">Services</a>
+        <a className="hover:text-blue-600" href="#">Pricing</a>
 
-        <div>
-          <a href="#">Megamenu</a>
-          <div className="hidden">
+        <div className="group relative min-w-[20px]">
+          <a className="hover:text-blue-600" href="#">Megamenu</a>
+          <div className="group-hover:visible flex absolute top-[25px] invisible flex-col justify-center items-start text-nowrap bg-gray-400 p-2 gap-2">
             <a href="#">Content Writing</a>
             <a href="#">Web Development</a>
             <a href="#">Marketing</a>
@@ -22,9 +22,9 @@ export default function Header() {
           </div>
         </div>
 
-        <a href="#">Blog</a>
-        <a href="#">Contact</a>
-        <button>Try free</button>
+        <a className="hover:text-blue-600" href="#">Blog</a>
+        <a className="hover:text-blue-600" href="#">Contact</a>
+        <button className="w-[90px] h-[30px] rounded-full bg-blue-600 hover:bg-orange-500 transition duration-200 text-white">Try free</button>
       </nav>
     </header>
   )
